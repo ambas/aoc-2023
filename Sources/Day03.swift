d78a17457d98fe7916464b3a625680a2d56e0f48
Ambas <ambaschbsanti@gmail.com>
2023-12-03 13:25:12 +0700

    Finish part 2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -1,8 +1,116 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ambas Chobsanti on 3/12/2566 BE.
-//
 
-import Foundation
+import Algorithms
+
+struct Day03: AdventDay {
+  // Save your data in a corresponding text file in the `Data` directory.
+  var data: String
+  
+  init(data: String) {
+    self.data = data
+    self.entities = data.split(separator: "\n").map {
+      Array($0) + ["."]
+    }
+  }
+  
+  // Splits input data into its component parts and convert from string.
+  let entities: [[Character]]
+  
+  var rowCount: Int {
+    entities.count
+  }
+  
+  var colCount: Int {
+    entities[0].count
+  }
+  
+  func isSymbol(_ row: Int, _ col: Int) -> Bool {
+    !entities[row][col].isNumber && entities[row][col] != "."
+  }
+  
+  func symbolAround(_ row: Int, _ startIndex: Int, _ endIndex: Int) -> [[Int]] {
+    var symbolPoints = [[Int]]()
+    let aboveRow = row - 1
+    let belowRow = row + 1
+    if aboveRow >= 0 {
+      let (startIndex, endIndex) = (startIndex - 1, endIndex + 1)
+      for col in startIndex..<endIndex where col >= 0 && col < colCount {
+        if isSymbol(aboveRow, col) {
+          symbolPoints.append([aboveRow, col])
+        }
+      }
+    }
+    if belowRow < rowCount {
+      let (startIndex, endIndex) = (startIndex - 1, endIndex + 1)
+      for col in startIndex..<endIndex where col >= 0 && col < colCount {
+        if isSymbol(belowRow, col) {
+          symbolPoints.append([belowRow, col])
+        }
+      }
+    }
+    if startIndex > 0 && isSymbol(row, startIndex - 1) {
+      symbolPoints.append([row, startIndex - 1])
+    }
+    if endIndex < (colCount - 2) && isSymbol(row, endIndex) {
+      symbolPoints.append([row, endIndex])
+    }
+    return symbolPoints
+  }
+  
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Any {
+    var res = 0
+    for row in 0..<rowCount {
+      var startIndex: Int?
+      for col in 0..<colCount {
+        let currChar = entities[row][col]
+        if startIndex == nil && currChar.isNumber {
+          startIndex = col
+        } else if startIndex != nil && (!currChar.isNumber) {
+          let num = Int(String(entities[row][startIndex!..<col]))!
+          if !symbolAround(row, startIndex!, col).isEmpty {
+            res += num
+          }
+          
+          startIndex = nil
+        }
+      }
+    }
+    return String(res)
+  }
+  
+  
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    var dict = [[Int]: [Int]]()
+    for row in 0..<rowCount {
+      var startIndex: Int?
+      for col in 0..<colCount {
+        let currChar = entities[row][col]
+        if startIndex == nil && currChar.isNumber {
+          startIndex = col
+        } else if startIndex != nil && (!currChar.isNumber) {
+          let num = Int(String(entities[row][startIndex!..<col]))!
+          symbolAround(row, startIndex!, col)
+            .filter {
+              let (row, col) = ($0[0], $0[1])
+              return entities[row][col] == "*"
+            }
+            .forEach {
+              let (row, col) = ($0[0], $0[1])
+              dict[[row, col], default: []].append(num)
+            }
+          startIndex = nil
+        }
+        
+        
+      }
+    }
+   
+    var res = 0
+    for value in dict.values where value.count == 2 {
+        res += (value[0] * value[1])
+    }
+    return String(res)
+  }
+  
+}
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -1,8 +1,38 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ambas Chobsanti on 3/12/2566 BE.
-//
+import XCTest
 
-import Foundation
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day03Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testDataPart1 = """
+    467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..
+    """
+  let testDataPart2 = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """
+  func testPart1() throws {
+    let challenge = Day03(data: testDataPart1)
+    XCTAssertEqual(String(describing: challenge.part1()), "4361")
+  }
+
+  func testPart2() throws {
+    let challenge = Day03(data: testDataPart1)
+    XCTAssertEqual(String(describing: challenge.part2()), "467835")
+  }
+
+}
