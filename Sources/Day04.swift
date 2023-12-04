d15b6da60572ba561c216ce6ed4984dba6aab966
Ambas <ambaschbsanti@gmail.com>
2023-12-04 12:55:03 +0700

    Clean up

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -1,8 +1,65 @@
-//
-//  File.swift
-//  
-//
-//  Created by Ambas Chobsanti on 4/12/2566 BE.
-//
-
 import Foundation
+import Algorithms
+
+struct Day04: AdventDay {
+  // Save your data in a corresponding text file in the `Data` directory.
+  var data: String
+  
+  struct Card {
+    let win: [Int]
+    let curr: [Int]
+    
+    init(_ input: String) {
+      let splited = input.split(separator: "|")
+      win = splited[0].split(separator: " ", omittingEmptySubsequences: true).compactMap { Int($0) }
+      curr = splited[1].split(separator: " ", omittingEmptySubsequences: true).compactMap { Int($0) }
+    }
+    
+    var calSame: Int {
+      let set1 = Set(win)
+      let set2 = Set(curr)
+      let same = set1.intersection(set2)
+      return same.count
+    }
+    
+    var calWin: Int {
+      let res = pow(Double(2), Double(calSame) - 1)
+      
+      return Int(res)
+      
+    }
+  }
+  
+  // Splits input data into its component parts and convert from string.
+  let entities: [Card]
+  
+  init(data: String) {
+    self.data = data
+    self.entities = data
+      .split(separator: "\n")
+      .map {
+        let splited = $0.split(separator: ":")
+        return Card(String(splited[1]))
+      }
+  }
+  
+  // Replace this with your solution for the first part of the day's challenge.
+  func part1() -> Any {
+    entities.map(\.calWin).reduce(0, +)
+  }
+  
+  
+  // Replace this with your solution for the second part of the day's challenge.
+  func part2() -> Any {
+    var numberOfCard = Array(repeating: 1, count: entities.count)
+    for (index, card) in entities.enumerated() {
+      let rightBound = min(index + card.calSame, entities.count)
+      for i in (index + 1)..<rightBound + 1 {
+        numberOfCard[i] += numberOfCard[index]
+      }
+    }
+    let res = numberOfCard.reduce(0, +)
+    return "\(res)"
+  }
+  
+}
--- Tests/Day04.swift
+++ Tests/Day04.swift
@@ -1,35 +1,28 @@
-//
-//  Day04.swift
-//  
-//
-//  Created by Ambas Chobsanti on 4/12/2566 BE.
-//
-
 import XCTest
 
-final class Day04: XCTestCase {
+@testable import AdventOfCode
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day04Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testDataPart1 = """
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """
+  
+  func testPart1() throws {
+    let challenge = Day04(data: testDataPart1)
+    XCTAssertEqual(String(describing: challenge.part1()), "13")
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  func testPart2() throws {
+    let challenge = Day04(data: testDataPart1)
+    XCTAssertEqual(String(describing: challenge.part2()), "30")
+  }
 
 }
